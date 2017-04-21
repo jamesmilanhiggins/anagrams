e2c070ebe4b84ada9b8c21adaef2291a19c6f695
James Higgins <james.milan.higgins@gmail.com>
2017-04-21 15:23:15 -0700

    add sinatra integration and pass test

--- app.rb
+++ app.rb
@@ -9,6 +9,12 @@ get('/') do
 end
 
 get('/anagrams') do#place form 'action' in the get parentheses
- @results1 = params.fetch('anagrams1').anagrams?() #from the name attribute in the input tag on the form
+
+ first_input = params.fetch('anagrams1')
+ second_input = params.fetch('anagrams2')
+
+
+ @results = first_input.anagrams?(second_input)
+ # .anagrams?() #from the name attribute in the input tag on the form
  erb(:result)#this is the page where you want your method to display
 end
