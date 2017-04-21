1d4c67af72c9ecfce81c58e14fa68acbdceb87e2
James Higgins <james.milan.higgins@gmail.com>
2017-04-21 14:57:46 -0700

    add additional code for sintra files

--- spec/anagrams_integration_spec.rb
+++ spec/anagrams_integration_spec.rb
@@ -7,7 +7,7 @@ set(:show_exceptions, false)
 describe('anagrams', {:type => :feature}) do
   it('alerts user if their word is an anagram') do
     visit('/') #wherever you have form inputs (our index page in this example)
-    fill_in('anagram', :with => "These words are anagrams.") #name attribute from input tag
+    fill_in('anagram', :with => "ruby", "bury") #name attribute from input tag
     click_button('click here') #add what ever is in between button tags example click me or submit or send
     expect(page).to have_content("These words are anagrams.") #must equal expected outcome, this is integration test
   end
