1f0f2999448d7c4790c911b0f7de7ec2be7fb92d
James Higgins <james.milan.higgins@gmail.com>
2017-04-21 08:49:08 -0700

    add third unit test so it splits string and sorts the letters alapgabetically and pass

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -3,7 +3,7 @@ class String
   define_method(:anagrams) do
 
   letters_array = self.split("")
-  letters_array
+  letters_array.sort
 
   end
 end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -6,8 +6,12 @@ describe('anagrams') do
   #   expect(("bury").anagrams()).to(eq("bury"))
   #
   # end
-  it(" splits a string into an array with each letter") do #doesnt matter english explination
-    expect(("bury").anagrams()).to(eq(["b","u","r","y"]))
+  # it(" splits a string into an array with each letter") do #doesnt matter english explination
+  #   expect(("bury").anagrams()).to(eq(["b","u","r","y"]))
+  #
+  # end
+  it(" splits a string into an array with each letter and sorts it alaphabetically") do #doesnt matter english explination
+    expect(("bury").anagrams()).to(eq(["b","r","u","y"]))
 
   end
   # it(" finds if two strings are anagrams") do #doesnt matter english explination
